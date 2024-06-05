fa5003c9dde103bc98d678a8f81c120cdea60f47
Prorock12 <bgliva33@gmail.com>
2024-06-05 14:49:23 +0200

    migration

---
+++ InterviewWebAPI/Migrations/20240514090912_initialmigration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InterviewWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vacancies",
+                columns: table => new
+                {
+                    VacancyId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProposalCompensation = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vacancies", x => x.VacancyId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vacancies");
+        }
+    }
+}
---
+++ InterviewWebAPI/Migrations/20240514094210_seedData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace InterviewWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Vacancies",
+                columns: new[] { "VacancyId", "Description", "ProposalCompensation", "Title" },
+                values: new object[,]
+                {
+                    { 1, "John", 30000m, "Developer" },
+                    { 2, "Chris", 50000m, "Manager" },
+                    { 3, "Mukesh", 20000m, "Consultant" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Vacancies",
+                keyColumn: "VacancyId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Vacancies",
+                keyColumn: "VacancyId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Vacancies",
+                keyColumn: "VacancyId",
+                keyValue: 3);
+        }
+    }
+}
